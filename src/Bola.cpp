829c90726e0379281875057c2d44d96fe84d7276
Sakaa-Eng <justlamm2@gmail.com>
2025-12-15 10:37:20 +0700

    Add files via upload

--- src/Bola.cpp
+++ src/Bola.cpp
@@ -2,25 +2,36 @@
 
 Bola::Bola(float x, float y, float radius, sf::Vector2f vel) {
     shape.setRadius(radius);
-    shape.setFillColor(sf::Color::White);
+    shape.setOrigin(radius, radius);
     shape.setPosition(x, y);
     velocity = vel;
 }
 
-void Bola::update(float dt, sf::RenderWindow &window) {
-    sf::Vector2f pos = shape.getPosition();
-    pos += velocity * dt;
+void Bola::update(float dt, const sf::RenderWindow& window) {
+    // Gerakkan berdasarkan velocity * dt
+    shape.move(velocity * dt);
 
+    float x = shape.getPosition().x;
+    float y = shape.getPosition().y;
     float r = shape.getRadius();
-    sf::Vector2u win = window.getSize();
+    float w = (float)window.getSize().x;
+    float h = (float)window.getSize().y;
 
-    if (pos.x < 0 || pos.x + 2*r > win.x) {
-        velocity.x *= -1;
+    // Bounce kiri/kanan (set position ke batas agar tidak terjebak)
+    if (x - r < 0.0f) {
+        shape.setPosition(r, y);
+        velocity.x = std::abs(velocity.x);
+    } else if (x + r > w) {
+        shape.setPosition(w - r, y);
+        velocity.x = -std::abs(velocity.x);
     }
 
-    if (pos.y < 0 || pos.y + 2*r > win.y) {
-        velocity.y *= -1;
+    // Bounce atas/bawah
+    if (y - r < 0.0f) {
+        shape.setPosition(x, r);
+        velocity.y = std::abs(velocity.y);
+    } else if (y + r > h) {
+        shape.setPosition(x, h - r);
+        velocity.y = -std::abs(velocity.y);
     }
-
-    shape.setPosition(pos);
 }
